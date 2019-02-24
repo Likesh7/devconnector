993123866c728202720fa5c5418bd60ca488e9a7
likesh shrestha <likeshrestha7@gmail.com>
2019-02-25 01:29:01 +0545

    experience form and functionality add

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -17,6 +17,11 @@ const Navbar = props => {
   const authLinks = (
     <ul className="navbar-nav ml-auto">
       <li className="nav-item">
+        <Link className="nav-link" to="/dashboard">
+          Dashboard
+        </Link>
+      </li>
+      <li className="nav-item">
         <a href="" onClick={onLogoutClick} className="nav-link">
           <img
             className="rounded-circle"
